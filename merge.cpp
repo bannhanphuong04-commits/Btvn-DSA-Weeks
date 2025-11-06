b31c6398ccd7eb40245f53ca34fbf9700039c3f8
Duong The Anh <bannhanphuong04@gmail.com>
2025-11-06 23:02:04 +0700

    merge.cpp

--- merge.cpp
+++ merge.cpp
@@ -1,43 +1,23 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
-
-void merge(int a[], int l, int m, int r) {
-    vector<int>x(a + l, a + m + 1);
-    vector<int>y(a + m + 1, a + r + 1);
-    int i = 0, j = 0;
-
-    while (i < x.size() && j < y.size()) {
-        if (x[i] < y[j]) {
-            a[l] = x[i];
-            ++l; ++i;
-        }
-        else {
-            a[l] = y[j];
-            ++l; ++j;
-        }
-    while (i < x.size()) {
-            a[l] = x[i];
-            ++l; ++i;
-        }
-    while (j < y.size()) {
-            a[l] = y[j];
-            ++l; ++j;
-        }
-    }
-}
-void merge_sort(int a[], int l, int r) {
-    if (l >= r) return;
-    int m = (l + r) / 2;
-    merge_sort(a, l, m);
-    merge_sort(a, m + 1, r);
-    merge(a, l, m, r);
-}
-int main() {
-    int a[100] = { 3,10,1,23,103,34,17,53,64,71 },n;
-    cin >> n;
-    merge_sort(a, 0, n - 1);
+void printArray(int a[], int n) {
     for (int i = 0; i < n; i++)
         cout << a[i] << " ";
-    return 0;
+    cout << endl;
 }
+void merge(int a[], int l, int m, int r, int n) {
+    vector<int> x(a + l, a + m + 1);
+    vector<int> y(a + m + 1, a + r + 1);
+    int i = 0, j = 0, k = l;
+    while (i < x.size() && j < y.size()) {
+        if (x[i] < y[j])
+            a[k++] = x[i++];
+        else
+            a[k++] = y[j++];
+    }
+    while (i < x.size())
+        a[k++] = x[i++];
+    while (j < y.size())
+        a[k++] = y[j++];
+    cout << "Sau khi gop [" << l << ", " << m << "] va [" << m +]()
